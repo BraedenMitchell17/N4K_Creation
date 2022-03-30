51bef0e195ad3f1035bbfc55ed0d5af1dd7b469c
Braeden Mitchell <Braeden.Mitchell@mymhc.ca>
2022-03-30 15:01:10 -0600

    prototype 5 integration

--- NFTAppV9/NFTAppV9.Android/MainActivity.cs
+++ NFTAppV9/NFTAppV9.Android/MainActivity.cs
@@ -11,12 +11,15 @@ namespace NFTAppV9.Droid
     [Activity(Label = "NFTAppV9", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static MainActivity Instance; // mwh
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Instance = this; // mwh
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
